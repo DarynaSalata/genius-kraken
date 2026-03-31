0dcacc5e03ccb8d127f9bd6be2f3531518c775c7
Daryna Salata <dasha.salata@icloud.com>
2026-03-31 13:14:22 +0200

    Lesson18
    Added new styles and adaptivity

--- js/modal.js
+++ js/modal.js
@@ -3,5 +3,6 @@ const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
+
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
